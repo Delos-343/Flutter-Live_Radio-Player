f56000b053ebde8ee3f4aa563dc4a5082ca068ba
Delos-343 <mfachry.handoko@gmail.com>
2023-02-26 17:26:02 +0700

    setup + completed new models: Song, Playlist

---
+++ lib/models/playlist_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaylistModel extends StatelessWidget {
+  const PlaylistModel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
 
+class SongModel extends StatelessWidget {
+  const SongModel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
