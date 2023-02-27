47857fede865bd81827c1f57448b9c2e7fa899c3
Delos-343 <mfachry.handoko@gmail.com>
2023-02-27 17:45:10 +0700

    finished playlist_model

--- lib/models/playlist_model.dart
+++ lib/models/playlist_model.dart
@@ -1,10 +1,34 @@
-import 'package:flutter/material.dart';
+import 'radio_model.dart';
 
-class PlaylistModel extends StatelessWidget {
-  const PlaylistModel({super.key});
+class Playlist {
+  final String title;
+  final List<Radio> radiochannel;
+  final String imageUrl;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
+  Playlist({
+    required this.title,
+    required this.radiochannel,
+    required this.imageUrl,
+  });
+
+  static List<Playlist> playlists = [
+    Playlist(
+      title: 'Hip-hop R&B Mix',
+      radiochannel: Radio.radiochannel,
+      imageUrl:
+          'https://images.unsplash.com/photo-1576525865260-9f0e7cfb02b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80',
+    ),
+    Playlist(
+      title: 'Rock & Roll',
+      radiochannel: Radio.radiochannel,
+      imageUrl:
+          'https://images.unsplash.com/photo-1629276301820-0f3eedc29fd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2848&q=80',
+    ),
+    Playlist(
+      title: 'Techno',
+      radiochannel: Radio.radiochannel,
+      imageUrl:
+          'https://images.unsplash.com/photo-1594623930572-300a3011d9ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+    )
+  ];
 }
