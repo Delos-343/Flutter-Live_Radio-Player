754973bbc58f9aa642ec4f4ec51d5fdc33005484
Delos-343 <mfachry.handoko@gmail.com>
2023-02-27 17:42:45 +0700

    finished radio_model

--- lib/models/radio_model.dart
+++ lib/models/radio_model.dart
@@ -1,10 +1,34 @@
-import 'package:flutter/material.dart';
+class Radio {
+  final String title;
+  final String description;
+  final String url;
+  final String coverUrl;
 
-class RadioModel extends StatelessWidget {
-  const RadioModel({super.key});
+  Radio({
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.coverUrl,
+  });
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
+  static List<Radio> radiochannel = [
+    Radio(
+      title: 'Glass',
+      description: 'Glass',
+      url: 'assets/music/glass.mp3',
+      coverUrl: 'assets/images/glass.jpg',
+    ),
+    Radio(
+      title: 'Illusions',
+      description: 'Illusions',
+      url: 'assets/music/illusions.mp3',
+      coverUrl: 'assets/images/illusions.jpg',
+    ),
+    Radio(
+      title: 'Pray',
+      description: 'Pray',
+      url: 'assets/music/pray.mp3',
+      coverUrl: 'assets/images/pray.jpg',
+    )
+  ];
 }
