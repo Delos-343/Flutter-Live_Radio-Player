a5b03cdce2dfa052dde480d403cfad241dd83d4d
Delos-343 <mfachry.handoko@gmail.com>
2023-02-27 17:27:05 +0700

    added dependencies as imports into radio_screen.dart

--- lib/screens/radio_screen.dart
+++ lib/screens/radio_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:just_audio/just_audio.dart';
+import 'package:rxdart/rxdart.dart' as rxdart;
+
+import '../models/song_model.dart';
+import '../widgets/widgets.dart';
 
 class RadioScreen extends StatelessWidget {
   const RadioScreen({super.key});
