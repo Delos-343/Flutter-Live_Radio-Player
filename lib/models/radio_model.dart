db754cf30920eac7a28bf066807f32a6bd5159e6
Delos-343 <mfachry.handoko@gmail.com>
2023-02-27 23:11:51 +0700

    added music + image assets

--- lib/models/radio_model.dart
+++ lib/models/radio_model.dart
@@ -16,21 +16,23 @@ class RadioChannel {
       title: 'Butlerian Jihad',
       // title: 'Prambors FM / Jakarta',
       description: '116399',
+      // description: '116399',
       url: 'assets/music/Dune OST - Ripples in the Sand (Hans Zimmer).mp3',
       // url: 'http://masima.rastream.com/masima-pramborsjakarta?',
-      coverUrl: 'assets/images/glass.jpg',
+      coverUrl: 'assets/images/arrakeen.jpg',
     ),
     RadioChannel(
-      title: 'HMTK',
-      description: 'B201',
-      url: 'assets/music/testvideo-2.mp3',
-      coverUrl: 'assets/images/illusions.jpg',
+      title: 'Voice of the Gravitar',
+      description: 'Y2323',
+      url: 'assets/music/The Expanse OST - To Ilus (Clinton Shorter).mp3.mp3',
+      coverUrl: 'assets/images/slow-zone.jpg',
     ),
     RadioChannel(
-      title: 'TekFis',
+      title: 'Khar-Toba',
       description: '2019',
-      url: 'assets/music/testvideo-3.mp3',
-      coverUrl: 'assets/images/pray.jpg',
+      url:
+          'assets/music/Homeworld RM OST - Assault on Chimera (Paul Ruskay).mp3',
+      coverUrl: 'assets/images/hw3-gate.png',
     )
   ];
 }
