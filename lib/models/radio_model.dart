147e23e822818a8ea039b68d7ce64cb848eea7b8
Delos-343 <mfachry.handoko@gmail.com>
2023-02-27 19:28:45 +0700

    prepping assets directory

--- lib/models/radio_model.dart
+++ lib/models/radio_model.dart
@@ -15,19 +15,19 @@ class RadioChannel {
     RadioChannel(
       title: 'HimaTekkom',
       description: 'B201',
-      url: 'assets/music/glass.mp3',
+      url: 'assets/music/testvideo-1.mp3',
       coverUrl: 'assets/images/glass.jpg',
     ),
     RadioChannel(
       title: 'HMTC',
       description: 'IF2020',
-      url: 'assets/music/illusions.mp3',
+      url: 'assets/music/testvideo-2.mp3',
       coverUrl: 'assets/images/illusions.jpg',
     ),
     RadioChannel(
       title: 'TekFis',
       description: '2019',
-      url: 'assets/music/pray.mp3',
+      url: 'assets/music/testvideo-3.mp3',
       coverUrl: 'assets/images/pray.jpg',
     )
   ];
